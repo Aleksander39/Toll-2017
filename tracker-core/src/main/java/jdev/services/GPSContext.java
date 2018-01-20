024dfe1e0173ca15319c7d207180251e9e406839
Aleksander39 <abrazumov.alexander@yandex.ru>
2018-01-20 15:38:27 +0700

    ++ @Bean в GPSContext

--- tracker-core/src/main/java/jdev/services/GPSContext.java
+++ tracker-core/src/main/java/jdev/services/GPSContext.java
@@ -1,7 +1,21 @@
 package jdev.services;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GPSContext {
+    @Bean
+    GPSService gpsService(){
+        return new GPSService();
+    }
+    @Bean
+    SendService sendService(){
+        return new SendService();
+    }
+    @Bean
+    StorageService storageService(){
+        return new StorageService();
+    }
+
 }
