2fe917084ff81301e6feb25681e125a8d1b55988
Aleksander39 <abrazumov.alexander@yandex.ru>
2018-01-20 15:40:14 +0700

    ++ context  в Main

--- tracker-core/src/main/java/jdev/services/GPSMain.java
+++ tracker-core/src/main/java/jdev/services/GPSMain.java
@@ -1,7 +1,11 @@
 package jdev.services;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class GPSMain {
     public static void main(String...args){
+        ApplicationContext context= new AnnotationConfigApplicationContext(GPSContext.class);
 
     }
 }
