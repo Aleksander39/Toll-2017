36712dbe8a073555ad61df2bb7fb7fc73d32a50b
Aleksander39 <abrazumov.alexander@yandex.ru>
2018-01-20 15:14:56 +0700

    ++Main.class для демонстрации запуска

---
+++ tracker-core/src/main/java/jdev/tracker/core/Main.java
@@ -0,0 +1,18 @@
+package jdev.tracker.core;
+
+import jdev.dto.PointDTO;
+
+/**
+ * Created by jdev on 07.03.2017.
+ */
+public class Main {
+    public static void main(String... args) throws Exception {
+        for (int i=0; i<5; i++) {
+            System.out.println("Main.main say Hello!!!!");
+            PointDTO pointDTO = new PointDTO();
+            pointDTO.setLat(45);
+            System.out.println(pointDTO.toJson());
+            Thread.sleep(1000);
+        }
+    }
+}
