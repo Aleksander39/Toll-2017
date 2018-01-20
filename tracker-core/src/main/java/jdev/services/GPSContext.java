47d2952ef897967e3c77993c5ec1f8f0ffc978a7
Aleksander39 <abrazumov.alexander@yandex.ru>
2018-01-20 17:05:04 +0700

    ++ TaskScheduler в GPSContext

--- tracker-core/src/main/java/jdev/services/GPSContext.java
+++ tracker-core/src/main/java/jdev/services/GPSContext.java
@@ -3,7 +3,9 @@ package jdev.services;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @Configuration
 @EnableScheduling
@@ -20,6 +22,13 @@ public class GPSContext {
     @Bean
     StorageService storageService(){
         return new StorageService();
+    }
+    @Bean
+    public TaskScheduler poolScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setThreadNamePrefix("poolScheduler");
+        scheduler.setPoolSize(20);
+        return scheduler;
     }
 
 }
