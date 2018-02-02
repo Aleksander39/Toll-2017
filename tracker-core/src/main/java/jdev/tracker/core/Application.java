35fa83a0bc96b83c08109aaf5f9fbb8c26c31712
Aleksander39 <abrazumov.alexander@yandex.ru>
2018-02-02 19:02:05 +0700

    ++аннотации

--- tracker-core/src/main/java/jdev/tracker/core/Application.java
+++ tracker-core/src/main/java/jdev/tracker/core/Application.java
@@ -1,4 +1,25 @@
 package jdev.tracker.core;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableScheduling
+@PropertySource("classpath:/app.properties")
+@ComponentScan({"jdev.services","jdev.tracker.core"})
 public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
 }
