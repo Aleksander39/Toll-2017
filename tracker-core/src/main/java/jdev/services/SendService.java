4e774c32d95c3f59c0cd5c7d7f6cc447e6aebef7
Aleksander39 <abrazumov.alexander@yandex.ru>
2018-02-02 18:41:00 +0700

    ++RestTemplate,postForObject

--- tracker-core/src/main/java/jdev/services/SendService.java
+++ tracker-core/src/main/java/jdev/services/SendService.java
@@ -2,11 +2,13 @@ package jdev.services;
 
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import jdev.dto.PointDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.PostConstruct;
 
@@ -17,14 +19,15 @@ public class SendService {
     @Autowired
     GPSService gpsService;
 
+    @Autowired
+    RestTemplate restTemplate;
+
     private static final Logger log = LoggerFactory.getLogger(SendService.class);
-    private long previous;
+
 
     @Scheduled(cron = "${cron.prop}")
-    public void send() throws InterruptedException {
-        long current = System.currentTimeMillis();
-        log.info((current - previous)+ storageService.take());
-        previous = current;
+    public PointDTO send(PointDTO pointDTO){
+        return  restTemplate.postForObject("http://localhost:8080/coords",pointDTO,PointDTO.class);
     }
     @PostConstruct
     @Scheduled(cron = "${cron.prop.gps}")
