5d6e3f0aecd51cf9cbf39f1325fbcd30b20ff137
Aleksander39 <abrazumov.alexander@yandex.ru>
2018-02-02 18:54:02 +0700

    ++ метод получения,@RequestBody( преобраз.запроса в java объект)@RequestMapping,@ResponseBody

--- server-core/src/main/java/jdev/controllers/TrackerController.java
+++ server-core/src/main/java/jdev/controllers/TrackerController.java
@@ -1,9 +1,21 @@
 package jdev.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import jdev.dto.PointDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TrackerController {
+    Logger logger = LoggerFactory.getLogger(TrackerController.class);
 
+    @RequestMapping(value = "/coords", method = RequestMethod.POST, headers = "Accept=application/json")
+    @ResponseBody
+     public PointDTO receive(@RequestBody PointDTO pointDTO){
+        logger.info(String.valueOf(pointDTO));
+
+        return pointDTO;
+
+     }
 
 }
