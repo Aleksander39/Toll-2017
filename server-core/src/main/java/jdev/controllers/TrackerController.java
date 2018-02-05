b04a73291b37b263beedbb78abd694ca2d848cd1
Aleksander39 <abrazumov.alexander@yandex.ru>
2018-02-06 00:48:10 +0700

    new TrackerController,RecFileService(запись координат в файл)

--- server-core/src/main/java/jdev/controllers/TrackerController.java
+++ server-core/src/main/java/jdev/controllers/TrackerController.java
@@ -1,31 +1,43 @@
 package jdev.controllers;
 
+
+import jdev.dto.PointDTO;
+import jdev.services.RecFileService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.FileWriter;
 import java.io.IOException;
 
 @RestController
 public class TrackerController {
+    @Autowired
+    RecFileService recFileService;
 
     Logger logger = LoggerFactory.getLogger(TrackerController.class);
+    private long previous;
 
-    @RequestMapping(value = "/coords", method = RequestMethod.POST, headers = "Accept=application/json")
+    @RequestMapping(value = "point", method = RequestMethod.POST)
     @ResponseBody
 
      public PointDTO receive(@RequestBody PointDTO pointDTO) throws IOException {
-        logger.info(String.valueOf(pointDTO));
-        try(FileWriter writer = new FileWriter("track.txt", true)) {
-            writer.write(String.valueOf(pointDTO));
-            writer.flush();
-        }
-        catch(IOException ex) {
-            logger.info(ex.getMessage());
-        }
-
-return pointDTO;
+        long current = System.currentTimeMillis();
+        logger.info((current - previous) +
+                        ",lat = " + pointDTO.getLat() +
+                        ",lon = " + pointDTO.getLon()+
+                        ",azimuth = "+pointDTO.getAzimuth()+
+                        ",instaSpeed = "+pointDTO.getInstaSpeed()+
+                        ", autoId = "+pointDTO.getAutoId()
+                );
+        recFileService.writeInFile((current - previous) + ",lat = " + pointDTO.getLat() +
+                ",lon = " + pointDTO.getLon()+
+                ",azimuth = "+pointDTO.getAzimuth()+
+                ",instaSpeed = "+pointDTO.getInstaSpeed()+
+                ", autoId = "+pointDTO.getAutoId());
+        return pointDTO;
      }
 
 
