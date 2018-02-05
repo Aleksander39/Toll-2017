5fb37ab2e7d8f126df9d975fe5c3d5061d04b600
Aleksander39 <abrazumov.alexander@yandex.ru>
2018-02-06 00:43:28 +0700

    видоизменяю StorageService

--- tracker-core/src/main/java/jdev/services/StorageService.java
+++ tracker-core/src/main/java/jdev/services/StorageService.java
@@ -1,18 +1,25 @@
 package jdev.services;
 
+
+import jdev.dto.PointDTO;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingDeque;
 
 @Service
 public class StorageService {
-    private BlockingQueue<String> queue = new LinkedBlockingDeque<>(100);
-    String take() throws InterruptedException {
+    private BlockingDeque<PointDTO> queue = new LinkedBlockingDeque<>(100);
+
+    PointDTO take() throws InterruptedException {
         return  queue.take();
     }
 
-    void put(String gpsPoint) throws InterruptedException {
+    void put(PointDTO gpsPoint) throws InterruptedException {
         queue.put(gpsPoint);
+    }
+    BlockingDeque<PointDTO> getAll() {
+        return queue;
     }
 }
