c448e0e146fa01b2ac6df0557e4aa4206e3a48bb
Aleksander39 <abrazumov.alexander@yandex.ru>
2018-02-06 12:18:46 +0700

    определяем путь

--- server-ui/src/main/java/jdev/security/Application.java
+++ server-ui/src/main/java/jdev/security/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("jd.config")
+@ComponentScan("jdev.config")
 public class Application {
 
     public static void main(String[] args) throws Throwable {
