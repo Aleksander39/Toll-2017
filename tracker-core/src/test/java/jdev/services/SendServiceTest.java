7af4873fdb6d25842ed7b3a0bb4f556b357c54dd
Aleksander39 <abrazumov.alexander@yandex.ru>
2018-02-13 16:31:34 +0700

    ++SendServiceTest

---
+++ tracker-core/src/test/java/jdev/services/SendServiceTest.java
@@ -0,0 +1,20 @@
+package jdev.services;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+@RunWith(MockitoJUnitRunner.class)
+public class SendServiceTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void send() {
+    }
+}
