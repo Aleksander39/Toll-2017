0b6411a0ecf240bf6d7b00822a92bb4ffd52c44d
Aleksander39 <abrazumov.alexander@yandex.ru>
2018-02-13 16:37:55 +0700

    тестирование SendServiceTest

--- tracker-core/src/test/java/jdev/services/SendServiceTest.java
+++ tracker-core/src/test/java/jdev/services/SendServiceTest.java
@@ -1,20 +1,42 @@
 package jdev.services;
 
+import jdev.dto.PointDTO;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestTemplate;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
 @RunWith(MockitoJUnitRunner.class)
 public class SendServiceTest {
+    @Mock
+    RestTemplate restTemplate;
+
+    private PointDTO pointDTO;
+    private String url ="http://localhost:8080/points";
+
 
     @Before
-    public void setUp() throws Exception {
-
+    public void setUp() throws Exception{
+        pointDTO = new PointDTO();
+        pointDTO.setLon(45.87);
+        pointDTO.setLat(56.98);
+        pointDTO.setAzimuth(345);
+        pointDTO.setInstaSpeed(30);
+        pointDTO.setAutoId("o567gfd");
+        pointDTO.setTime(System.currentTimeMillis());
     }
+
+
 
     @Test
     public void send() {
+        when(restTemplate.postForObject(url,pointDTO,PointDTO.class)).thenReturn(pointDTO)
+                .thenThrow(ResourceAccessException.class);
     }
 }
