834cf88893cce0f24e0b530288bd2d71d989fe3b
Aleksander39 <abrazumov.alexander@yandex.ru>
2018-02-13 16:07:16 +0700

    +++StorageServiceTest

---
+++ tracker-core/src/test/java/jdev/services/StorageServiceTest.java
@@ -0,0 +1,25 @@
+package jdev.services;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StorageServiceTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void take() {
+    }
+
+    @Test
+    public void put() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+}
