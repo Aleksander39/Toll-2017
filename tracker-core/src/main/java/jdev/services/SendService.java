baab7261dd4dea7ff1c362a972984d6c6e4e4837
Aleksander39 <abrazumov.alexander@yandex.ru>
2018-02-13 13:54:45 +0700

    редактирование SendService.class

--- tracker-core/src/main/java/jdev/services/SendService.java
+++ tracker-core/src/main/java/jdev/services/SendService.java
@@ -2,10 +2,11 @@ package jdev.services;
 
 import jdev.dto.PointDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
 import java.util.concurrent.BlockingDeque;
+
 
 @Service
 public class SendService {
@@ -14,24 +15,27 @@ public class SendService {
     RestTemplate restTemplate;
     @Autowired
     StorageService storageService;
+    String url = "http://localhost:8080/points/";
 
 
-    //@Scheduled(cron = "${cron.prop}")
-    public PointDTO[] send(PointDTO pointDTO) {
+
+    @Scheduled(cron = "${cron.prop}")
+    public PointDTO[] send() {
+        int i=0;
         BlockingDeque<PointDTO> queue = storageService.getAll();
-        PointDTO[] points = new PointDTO[queue.size()];
+        PointDTO[]pointDTOS = new PointDTO[queue.size()];
 
-        int i = 0;
-        for(PointDTO point : queue) {
-            points[i++] = point;
-            storageService.getAll().remove(point);
+        for(PointDTO pointDTO:queue){
+            pointDTOS[i++]=pointDTO;
+            storageService.getAll().remove(pointDTO);
+
         }
-
-       return restTemplate.postForObject("http//localhost:8080/point",pointDTO , PointDTO[].class);
+      return   restTemplate.postForObject(url, pointDTOS,PointDTO[].class);
 
     }
 
-
-
-
 }
+
+
+
+
