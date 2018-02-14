1dc21df3f1fc563968ac488502b4ce8340c4ae0d
Aleksander39 <abrazumov.alexander@yandex.ru>
2018-02-14 10:50:30 +0700

    изменения в TrackerController.class,app.properties

--- server-core/src/main/java/jdev/controllers/TrackerController.java
+++ server-core/src/main/java/jdev/controllers/TrackerController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -19,19 +20,24 @@ public class TrackerController {
     public TrackerController(@Autowired RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
-
     Logger logger = LoggerFactory.getLogger(TrackerController.class);
 
 
-    @RequestMapping(value = "/point/", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
+    @RequestMapping(value = "/points", method = RequestMethod.POST, consumes = "application/json")
     @ResponseBody
     public PointDTO[] createPoint(@RequestBody PointDTO[] pointDTO) throws IOException {
         for (int i = 0; i < pointDTO.length; i++) {
             logger.info(String.valueOf(pointDTO[i++]));
+            try{
+                BufferedWriter writer = new BufferedWriter(new FileWriter("track.txt",true));
+                writer.write(String.valueOf(pointDTO[i++]));
+                writer.newLine();
+                writer.flush();
+            }
+            catch(IOException ex) {
+                logger.info(ex.getMessage());
+            }
 
-            FileWriter writer = new FileWriter("track.txt", true);
-            writer.write(String.valueOf(pointDTO[i]));
-            writer.flush();
 
         }
             return pointDTO;
