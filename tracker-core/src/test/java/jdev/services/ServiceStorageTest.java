7fb866fb4a92df8a5892512c02b967ef6ee0dd53
Aleksander39 <abrazumov.alexander@yandex.ru>
2018-02-24 19:12:00 +0700

    тестирование

--- tracker-core/src/test/java/jdev/services/ServiceStorageTest.java
+++ tracker-core/src/test/java/jdev/services/ServiceStorageTest.java
@@ -1,4 +1,47 @@
 package jdev.services;
 
+import jdev.dto.PointDTO;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(MockitoJUnitRunner.class)
 public class ServiceStorageTest {
+    @InjectMocks
+    StorageService storageService;
+
+    @Before
+    public void setUp() throws Exception {
+        PointDTO pointDTO = new PointDTO();
+        pointDTO.setLon(45.87);
+        pointDTO.setLat(56.98);
+        pointDTO.setAzimuth(345);
+        pointDTO.setInstaSpeed(30);
+        pointDTO.setAutoId("o567gfd");
+        pointDTO.setTime(System.currentTimeMillis());
+        storageService.put(pointDTO);
+    }
+
+    @Test
+    public void take() throws InterruptedException {
+        PointDTO result = storageService.take();
+        assertNotNull(result);
+        assertEquals(56.98, result.getLat(), 0);
+        assertEquals(45.87, result.getLon(), 0);
+        assertEquals(345, result.getAzimuth(), 0);
+        assertEquals(30, result.getInstaSpeed(), 0);
+        assertEquals("o567gfd", result.getAutoId());
+    }
+
+
+
+    @Test
+    public void getAll() {
+        assertNotNull(storageService.getAll());
+    }
 }
