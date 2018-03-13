48e04af2ba54e07e4144ac6cf7405f172a7c0dd9
Aleksander39 <abrazumov.alexander@yandex.ru>
2018-03-13 22:40:12 +0700

    реализация RESTful сервис в server-core

--- server-core/src/main/java/jdev/controllers/TrackerController.java
+++ server-core/src/main/java/jdev/controllers/TrackerController.java
@@ -1,5 +1,7 @@
 package jdev.controllers;
 
+import jdev.dao.Point;
+import jdev.dao.repo.PointRepository;
 import jdev.dto.PointDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +12,8 @@ import org.springframework.web.client.RestTemplate;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 
 @RestController
@@ -20,30 +24,60 @@ public class TrackerController {
     public TrackerController(@Autowired RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
+
     Logger logger = LoggerFactory.getLogger(TrackerController.class);
 
+    @Autowired
+    PointRepository pointRepository;
 
-    @RequestMapping(value = "/points", method = RequestMethod.POST, consumes = "application/json")
+
+    @RequestMapping(value = "/points", method = RequestMethod.POST, headers = "Accept=application/json")
     @ResponseBody
     public PointDTO[] createPoint(@RequestBody PointDTO[] pointDTO) throws IOException {
         for (int i = 0; i < pointDTO.length; i++) {
             logger.info(String.valueOf(pointDTO[i]));
-            try{
-                BufferedWriter writer = new BufferedWriter(new FileWriter("track.txt",true));
+            try {
+                BufferedWriter writer = new BufferedWriter(new FileWriter("track.txt", true));
                 writer.write(String.valueOf(pointDTO[i]));
                 writer.newLine();
                 writer.flush();
-            }
-            catch(IOException ex) {
+            } catch (IOException ex) {
                 logger.info(ex.getMessage());
             }
 
 
         }
-            return pointDTO;
+        return pointDTO;
+    }
+
+    @RequestMapping(value = "/points", method = RequestMethod.GET, headers = "Accept=application/json")
+    @ResponseBody
+    public PointDTO[] getByTime(@RequestParam("auto-id") String autoId, @RequestParam long hours) {
+        long millis = TimeUnit.HOURS.toMillis(hours);
+        List<jdev.dao.Point> allPoints = (List<jdev.dao.Point>) pointRepository.filterByTime(
+                autoId,
+                millis,
+                System.currentTimeMillis()
+        );
+
+        int i = 0;
+        PointDTO[] points = new PointDTO[ allPoints.size()];
+        for (jdev.dao.Point entityPoint : allPoints) {
+            PointDTO point = new PointDTO();
+            point.setLat(entityPoint.getLat());
+            point.setLon(entityPoint.getLon());
+            point.setAzimuth(entityPoint.getAzimuth());
+            point.setInstaSpeed(entityPoint.getSpeed());
+            point.setTime(entityPoint.getTime());
+            point.setAutoId(entityPoint.getCar().getIdCar());
+
+            points[i++] = point;
         }
 
+        return points;
+
     }
+}
 
 
 
